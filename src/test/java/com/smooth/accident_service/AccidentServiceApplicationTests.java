22d6e6cdbcc6a53d17a02594ab568233972c37b7
Jiyoung Park <pjy2163@naver.com>
2025-09-02 16:39:44 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/smooth/accident_service/AccidentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smooth.accident_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccidentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
