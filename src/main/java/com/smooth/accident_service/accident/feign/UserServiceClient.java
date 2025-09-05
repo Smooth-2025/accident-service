16425d7a77b0ff69a01ba0f591d0ada0d3151af0
Jiyoung Park <pjy2163@naver.com>
2025-09-06 00:09:14 +0900

    feat: feign 서비스 구현

---
+++ src/main/java/com/smooth/accident_service/accident/feign/UserServiceClient.java
@@ -0,0 +1,12 @@
+package com.smooth.accident_service.accident.feign;
+
+import com.smooth.accident_service.accident.dto.UserDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "user-service", url = "${app.client.user-service.url}", path = "internal/v1/users")
+public interface UserServiceClient {
+    @GetMapping("/{userId}/admin-info")
+    UserDto getUserInfo(@PathVariable("userId") Long userId);
+}
