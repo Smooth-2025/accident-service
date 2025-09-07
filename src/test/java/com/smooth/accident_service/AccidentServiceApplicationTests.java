43b05745f2242d7a8c3890e726bd6cd280d60686
minju26 <mjalswn26@gmail.com>
2025-09-07 21:38:44 +0900

    test: 테스트용 설정 추가

--- src/test/java/com/smooth/accident_service/AccidentServiceApplicationTests.java
+++ src/test/java/com/smooth/accident_service/AccidentServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.smooth.accident_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestConfig.class)
 class AccidentServiceApplicationTests {
 
     @Test
