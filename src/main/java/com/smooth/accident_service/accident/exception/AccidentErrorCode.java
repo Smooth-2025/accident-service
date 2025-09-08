432469aed79b7b594195386ad181917ba66ec523
seohee-P <p03061180@gmail.com>
2025-09-08 17:17:56 +0900

    feat: 기간별 사고 조회 시 조회 끝 날짜가 시작 날짜보다 빠를 경우 예외 처리 추가

--- src/main/java/com/smooth/accident_service/accident/exception/AccidentErrorCode.java
+++ src/main/java/com/smooth/accident_service/accident/exception/AccidentErrorCode.java
@@ -11,6 +11,7 @@ public enum AccidentErrorCode implements ErrorCode {
 
     INVALID_PAGE_NUMBER(HttpStatus.BAD_REQUEST, 6400, "유효하지 않은 페이지 번호입니다."),
     INVALID_SCALE_VALUE(HttpStatus.BAD_REQUEST, 6401, "유효하지 않은 사고 등급입니다. (medium, high만 허용)"),
+    INVALID_DATE_RANGE(HttpStatus.BAD_REQUEST, 6402, "종료 날짜는 시작 날짜보다 이전일 수 없습니다."),
     ADMIN_ONLY_ACCESS(HttpStatus.FORBIDDEN, 6403, "관리자만 접근 가능합니다.");
 
     private final HttpStatus httpStatus;
